37ea8cc903dd73c44809af1f87fc91ed09f6b4b8
Haniel023 <54757754+Haniel023@users.noreply.github.com>
2021-03-02 14:17:16 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { BodyComponent } from './body/body.component';
+import { ScreenComponent } from './screen/screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    BodyComponent,
+    ScreenComponent
   ],
   imports: [
     BrowserModule,
